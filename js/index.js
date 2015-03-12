df78335ad00fe644b6642430e65b5a37bab92d63
Marius Petcu <dapetcu21@gmail.com>
2015-03-12 16:27:17 +0200

    First version that booted up on LFA 2

--- js/index.js
+++ js/index.js
@@ -1,76 +1,71 @@
-define([
-  'lfa-exercises',
-  'lfa-backstage',
-  'lfa-classroom',
-  'lfa-classroom/views/name-input',
-  'lfa-content-widgets',
-], function(Exercises) {
-  // Add content editor module
-  //$.getScript('/dist/lfa-content-editor.js');
+// Add content editor module
+//$.getScript('/dist/lfa-content-editor.js');
 
-  window.Exercises = Exercises;
-  NameInput = arguments[3];
+var Exercises = require('lfa-exercises');
+var NameInput = require('lfa-classroom/views/name-input');
+var App = require('lfa-core/app');
 
-  window.firstTimeDoneMessages = [
-    'Bravo!',
-    'Congrats!',
-    'Excellent!',
-    'Correct!',
-    'Super!',
-    'Impressive!',
-    'Flawless!'
-  ];
+window.Exercises = Exercises;
 
-  var greetings = ['Welcome!', 'Hi!', 'Good day!', 'Howdy!', 'What\'s up?'];
+window.firstTimeDoneMessages = [
+  'Bravo!',
+  'Congrats!',
+  'Excellent!',
+  'Correct!',
+  'Super!',
+  'Impressive!',
+  'Flawless!'
+];
 
-  var hh = (new Date()).getHours();
+var greetings = ['Welcome!', 'Hi!', 'Good day!', 'Howdy!', 'What\'s up?'];
 
-  if (hh >= 2 && hh < 12) {
-    greetings.push('Good morning!');
-  }
-  else if (hh > 17 || hh < 2) {
-    greetings.push('Good evening!!');
-  } else {
-    greetings.push('Hello!');
-  }
+var hh = (new Date()).getHours();
 
-  var useCounter = window.App.storage.getItem('useCounter') || 0;
-  useCounter = parseInt(useCounter) + 1;
+if (hh >= 2 && hh < 12) {
+  greetings.push('Good morning!');
+}
+else if (hh > 17 || hh < 2) {
+  greetings.push('Good evening!!');
+} else {
+  greetings.push('Hello!');
+}
 
-  var message = greetings[Math.min(Math.floor(Math.random() *
-    greetings.length*1.5), greetings.length-1)];
+var useCounter = App.storage.getItem('useCounter') || 0;
+useCounter = parseInt(useCounter) + 1;
+
+var message = greetings[Math.min(Math.floor(Math.random() *
+  greetings.length*1.5), greetings.length-1)];
 
 
-  if(useCounter < 2){
-     message = 'Thank you for trying this!';
-  }
+if(useCounter < 2){
+   message = 'Thank you for trying this!';
+}
 
-  window.setTimeout(function () {
-    window.App.trigger('avatar:mood', {
-      mood: 'smile',
-      message: message,
-      interval: 5000,
-      icon: 'fa-smile-o'
-    });
-  }, 4000);
-
-  window.App.storage.setItem('useCounter', useCounter);
-
-  var nameInput = null;
-
-  App.book.on('render', function (opts) {
-    var chapter = opts.chapter;
-
-    if (nameInput) {
-      nameInput.remove();/**/
-    }
-
-    if (chapter === 'ch00-cover-00-cover') {
-      nameInput = new NameInput({
-        el: $('.cover-name-input'),
-      });
-    }
-
-    $('.progress .progress-bar').progressbar();
+window.setTimeout(function () {
+  App.trigger('avatar:mood', {
+    mood: 'smile',
+    message: message,
+    interval: 5000,
+    icon: 'fa-smile-o'
   });
+}, 4000);
+
+App.storage.setItem('useCounter', useCounter);
+
+var nameInput = null;
+
+App.book.on('render', function (opts) {
+  var chapter = opts.chapter;
+
+  if (nameInput) {
+    nameInput.remove();/**/
+  }
+
+  if (chapter === 'ch00-cover-00-cover') {
+    nameInput = new NameInput({
+      el: $('.cover-name-input'),
+    });
+  }
+
+  $('.progress .progress-bar').progressbar();
 });
