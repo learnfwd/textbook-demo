53347e04d6fe95440309ae8c29276265a247068b
Iulian <savin.iulian90@gmail.com>
2015-09-30 14:37:15 +0300

    fixed some bugs with scrolling in bootstraptour - removed unnecessary files

--- assets/test.js
+++ assets/test.js
@@ -4,35 +4,47 @@ var tour = new Tour({
   {
     orphan: false,
     element: "#selector-exercises",
-    title: "Quick tip",
-    content: "Use the\'next\' and \'previous\' buttons to navigate",
-    placement: 'bottom'
+    title: "Quick navigation",
+    content: "Use the \'next\' and \'previous\' buttons to navigate",
+    placement: 'bottom', // top bottom right left
+    onShow: function (tour) {
+      $('#scrollview').animate({ scrollTop: $('#selector-exercises').offset().top }, 500);
+    }
   },
   {
     orphan: false,
     element: "#pick-one",
-    title: "Title of my step",
-    content: "Content of my step",
-    placement: 'left'
+    title: "Pick-one ",
+    content: "simple exercise, with multiple answers where only one is correct",
+    placement: 'right',
+    onShow: function (tour) {
+      $('#scrollview').animate({ scrollTop: $('#pick-one').offset().top }, 500);
+    }
   },
   {
     orphan: false,
     element: "#push-to-select",
-    title: "Title of my step",
-    content: "Content of my step",
-    placement: 'left'
+    title: "push to select",
+    content: "<img src=\'http://ecx.images-amazon.com/images/I/315dK1%2BTOFL.jpg\'></img>",
+    placement: 'top',
+    onShow: function (tour) {
+      $('#scrollview').animate({ scrollTop: $('#push-to-select').offset().top }, 500);
+    }
   },
   {
     orphan: false,
     element: "#true-false-exercises-or-true-false-",
-    title: "Title of my step",
-    content: "Content of my step",
-    placement: 'left'
+    title: "true / false",
+    content: "<img src= \'http://www.selvamadre.com/wp-content/uploads/2014/08/red-or-blue-pill.jpg\'></img>",
+    placement: 'right',
+    onShow: function (tour) {
+      $('#scrollview').animate({ scrollTop: $('#true-false-exercises-or-true-false-').offset().top }, 500);
+    }
   }
   ],
   storage:false
   });
 tour.init();
 tour.start(true);
-
-
+//$('#scrollview').animate({ scrollTop: $('#true-false-exercises-or-true-false-').offset().top }, 500);
+//$('button[data-role="next"]').css('background-color','red');
