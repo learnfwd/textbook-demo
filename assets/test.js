27a2072c3d91f038ded4b907ce163db07abcc475
Iulian <savin.iulian90@gmail.com>
2015-09-30 10:16:55 +0300

    testing bootstraptour

--- assets/test.js
+++ assets/test.js
@@ -2,24 +2,28 @@ var tour = new Tour({
   autoscroll: true,
   steps: [
   {
+    orphan: false,
     element: "#selector-exercises",
     title: "Quick tip",
     content: "Use the\'next\' and \'previous\' buttons to navigate",
     placement: 'bottom'
   },
   {
+    orphan: false,
     element: "#pick-one",
     title: "Title of my step",
     content: "Content of my step",
     placement: 'left'
   },
   {
+    orphan: false,
     element: "#push-to-select",
     title: "Title of my step",
     content: "Content of my step",
     placement: 'left'
   },
   {
+    orphan: false,
     element: "#true-false-exercises-or-true-false-",
     title: "Title of my step",
     content: "Content of my step",
